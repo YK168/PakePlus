3de99c18d486053352ebfa82dcbd2401fec43e39
谷南胖罗汉 <58681254+YK168@users.noreply.github.com>
2026-06-26 15:52:29 +0800

    update src file from pakeplus

---
+++ src/js/about.af550155.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkmy_project"]=self["webpackChunkmy_project"]||[]).push([[594],{5978:function(n,t,u){u.r(t),u.d(t,{default:function(){return i}});var e=u(6768);const r={class:"about"};function a(n,t){return(0,e.uX)(),(0,e.CE)("div",r,[...t[0]||(t[0]=[(0,e.Lk)("h1",null,"This is an about page",-1)])])}var c=u(1241);const s={},o=(0,c.A)(s,[["render",a]]);var i=o}}]);
+//# sourceMappingURL=about.af550155.js.map
